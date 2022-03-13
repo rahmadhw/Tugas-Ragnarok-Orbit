4c959acc639b5e027870176daf817f6036c753e4
rahmadhw <rahmadhidyat098@gmail.com>
2022-03-13 22:36:54 +0700

    Tugas Hari senin selasa rabu kamis selesai

---
+++ module.py
@@ -0,0 +1,12 @@
+def Panggilan():
+    print("Nama Saya Budiman")
+    
+    
+    
+def Hewan():
+    print("Saya jerapah")
+    
+    
+def Kalkulasi(a,b):
+    print(f'hasil pertambahan dari 5+5 = {a + b}')
+    
